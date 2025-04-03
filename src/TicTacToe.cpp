c7163c33b8ba85a4afed8ab891cd6ff2837e3804
Aaron Don <aarondon004@gmail.com>
2025-04-03 10:35:58 -0400

    show board implemnented

--- src/TicTacToe.cpp
+++ src/TicTacToe.cpp
@@ -1,13 +1,12 @@
 #include "TicTacToe.h"
 #include <cstdlib>
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-char board[3][3]; //Possible values are X, O and _ (for blank positions)
+char board[3][3]; // Possible values are X, O and _ (for blank positions)
 char player = 'X';
 int plays = 0;
 char winner;
-
 
 void clearScreen()
 {
@@ -17,4 +16,18 @@ void clearScreen()
 #else
 	system("clear");
 #endif
+}
+
+void showBoard()
+{
+	cout << "Current board:\n";
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << board[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
