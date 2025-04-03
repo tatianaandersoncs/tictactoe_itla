8ce5dffc4b1233f090bdb629336c1ee2e0dd503f
Junyao <junyaokh822@gmail.com>
2025-04-03 11:15:35 -0400

    updated showBoard function

--- src/TicTacToe.cpp
+++ src/TicTacToe.cpp
@@ -1,6 +1,6 @@
 #include "TicTacToe.h"
 #include <cstdlib>
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 char board[3][3]; //Possible values are X, O and _ (for blank positions)
@@ -8,13 +8,50 @@ char player = 'X';
 int plays = 0;
 char winner;
 
+void init()
+{
+    // Initialize the board with blank spaces
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            board[i][j] = '_';
+        }
+    }
+    
+    // Set initial values
+    player = 'X';
+    plays = 0;
+    winner = '\0';
+}
 
 void clearScreen()
 {
-	cout << "Check\n";
+    cout << "Check\n";
 #ifdef _WIN32
-	system("cls");
+    system("cls");
 #else
-	system("clear");
+    system("clear");
 #endif
 }
+
+void showBoard()
+{
+    cout << "\n";
+    cout << "   0   1   2  " << endl;  // Column numbers
+    cout << " +---+---+---+" << endl;
+    
+    for(int i = 0; i < 3; i++)
+    {
+        cout << i << "| ";  // Row number
+        for(int j = 0; j < 3; j++)
+        {
+            cout << board[i][j] << " | ";  // Cell content
+        }
+        cout << endl;
+        cout << " +---+---+---+" << endl;  // Row separator
+    }
+    
+    cout << "\nPlayer " << player << "'s turn." << endl;  // Show whose turn it is
+}
+
