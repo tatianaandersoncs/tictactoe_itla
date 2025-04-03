fb94857f4944936918912d0af7882fee75dccaad
tatianaandersoncs <101019509+tatianaandersoncs@users.noreply.github.com>
2025-04-03 09:09:36 -0400

    Update TicTacToe.h

--- src/TicTacToe.h
+++ src/TicTacToe.h
@@ -3,16 +3,18 @@
  *
  *  Created on: May 22, 2016
  *      Author: raydelto
+ *  Created on: Apr 4, 2025
+ *      Modified by: tatianaandersoncs
  */
 
 #ifndef TICTACTOE_H_
 #define TICTACTOE_H_
 
-void init();
-bool validate();
+void init(); // user story 1
+bool validateInput(); // user story 2
 void clearScreen();
-void showBoard();
-bool gameOver(); //implement this method
+void showBoard(); // user story 3
+bool isGameOver(); // user story 4
 
 
 
