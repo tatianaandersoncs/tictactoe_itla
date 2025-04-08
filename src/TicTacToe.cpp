e7275e31ec40b82a3bb65d5a5ed4f6db1db97685
ObbAndIbb <93671853+ObbAndIbb@users.noreply.github.com>
2025-04-08 09:46:36 -0400

    Andrew Li, Mykhailo Brykailo, Loay Elasmar, Salim Noma, ValidateInput function

--- src/TicTacToe.cpp
+++ src/TicTacToe.cpp
@@ -18,3 +18,22 @@ void clearScreen()
 	system("clear");
 #endif
 }
+
+bool ValidateInput(int row, int column)
+{ 
+	if(row > 1 || row > 3 || column > 1 || column > 3 )
+	{
+		cout << "This input is not within the board please try again!" << endl;
+		return false;
+	}
+
+	if(board[row][column]!= '_')
+	{ 
+		cout << "This spot is already taken please try again!" << endl;
+		return false; 
+	}
+
+	else return true;
+ 
+}
+
