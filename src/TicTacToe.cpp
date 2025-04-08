85eb23c8bb8a4d4400fe08a3eec0577058c6c6e3
Hawjean Ng Cen <hawjeanncen448@gmail.com>
2025-04-08 09:41:09 -0400

    Adding init to TicTacToe.cpp fil pick 9843442

--- src/TicTacToe.cpp
+++ src/TicTacToe.cpp
@@ -8,6 +8,17 @@ char player = 'X';
 int plays = 0;
 char winner;
 
+void init()
+{
+	for (int i = 0 ; i < 3 ; i++)
+	{
+		for (int j = 0 ; j < 3 ; j++)
+		{
+			board[i][j] = "_";
+		}
+	}
+}
+
 
 void clearScreen()
 {
@@ -18,5 +29,3 @@ void clearScreen()
 	system("clear");
 #endif
 }
-
-
